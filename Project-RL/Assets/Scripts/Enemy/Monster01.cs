b64cc4e68f36f607e8297b322c0431768003d1ec
Junustin <97379872+Junustin@users.noreply.github.com>
2022-10-10 21:45:55 +0700

    Save

--- Project-RL/Assets/Scripts/Enemy/Monster01.cs
+++ Project-RL/Assets/Scripts/Enemy/Monster01.cs
@@ -15,4 +15,29 @@ public class Monster01 : Enemy,IDamagable
         navMeshAgent=GetComponent<NavMeshAgent>();
         rb=GetComponent<Rigidbody>();
     }
+
+    private void FixedUpdate()
+    {
+        navMeshAgent.destination = playerRef.transform.position;//Move to player        
+    }
+
+    public override void TakeDamage(float Damage)
+    {
+        base.TakeDamage(Damage);        
+        Vector3 knockBackDir = (transform.position - playerRef.transform.position).normalized;
+        rb.AddForce(knockBackDir * 10, ForceMode.Impulse);
+        StartCoroutine("KnockBackTimer", 0.1f);
+
+        
+    }
+    IEnumerator KnockBackTimer(float time)
+    {
+        yield return new WaitForSeconds(time);
+        rb.velocity = Vector3.zero;
+    }
+
+    public override void Die()
+    {
+        base.Die();
+    }
 }
