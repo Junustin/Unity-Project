4ada70c05300e86416788efc8ae9ad47861185ec
Junustin <97379872+Junustin@users.noreply.github.com>
2022-10-21 18:47:54 +0700

    Update Spawner

--- Project-RL/Assets/Scripts/Manager/EnemySpawner.cs
+++ Project-RL/Assets/Scripts/Manager/EnemySpawner.cs
@@ -10,11 +10,20 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] GameObject batonEnemyPrefab;
 
     [SerializeField] float batonEnemyInterval;
+    [SerializeField] int numberToSpawn;
+    int currentSpawned;
+
+    [SerializeField] GameObject wall01;
+    [SerializeField] GameObject wall02;
+
+    
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            wall01.SetActive(true);
+            wall02.SetActive(true);
             StartCoroutine(SpawnEnemy(batonEnemyPrefab, batonEnemyInterval));
             Destroy(boxTrigger);
         }
@@ -22,9 +31,19 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator SpawnEnemy(GameObject enemy,float interval)
     {
-        yield return new WaitForSeconds(interval);
-        Instantiate(enemy, spawnPoint[Random.Range(0, spawnPoint.Length)].position, Quaternion.identity);
-        StartCoroutine(SpawnEnemy(enemy, interval));
+        if(currentSpawned < numberToSpawn)
+        {
+            yield return new WaitForSeconds(interval);            
+            Instantiate(enemy, spawnPoint[Random.Range(0, spawnPoint.Length)].position, Quaternion.identity);
+            currentSpawned++;
+            StartCoroutine(SpawnEnemy(enemy, interval));
+        }
+        else
+        {
+            StopAllCoroutines();
+            wall01.SetActive(false);
+            wall02.SetActive(false);
+        }
     }
 
 
